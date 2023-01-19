e142c103eb096cf657bbf3f1652964598d7df288
Leonardo Takashi Teramatsu <leonardo.teramatsu@gmail.com>
2023-01-19 19:04:58 -0300

    Explicando o conceito

--- secao2-JavaScript-Basico-Para-Iniciantes/Aula01-Console.log -A-primeira-coisa-que-voce-vai-ver-em-JS/Aula01-Console-log.js
+++ secao2-JavaScript-Basico-Para-Iniciantes/Aula01-Console.log -A-primeira-coisa-que-voce-vai-ver-em-JS/Aula01-Console-log.js
@@ -1,1 +1,2 @@
+// O ";" é opcional no JavaScript. Mas nesse curso, pelo professor, será colocado o ";" mais para o aluno aprender em qual momento é correto coloca-lo ou não.
 console.log('Hello WounderWorld!');
